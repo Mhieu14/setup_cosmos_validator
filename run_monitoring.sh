e8de4572dbba3fa6bf7974291b5dbcefeefe4fb7
buiminhhieuno5@gmail.com <buiminhhieuno5@gmail.com>
2022-12-05 17:58:16 +0700

    compress

--- run_monitoring.sh
+++ run_monitoring.sh
@@ -5,12 +5,12 @@ export VALIDATOR_IP=$2
 export PROM_PORT=${3:-26660}
 
 # create env
-rm -f cosmos_node_monitoring/.env
-echo -e $MONITERING_ENV_FILE > cosmos_node_monitoring/.env
+rm -f ~/cosmos_node_monitoring/.env
+echo -e $MONITERING_ENV_FILE > ~/cosmos_node_monitoring/.env
 
 # add prometheus prams
-/cosmos_node_monitoring/add_validator.sh VALIDATOR_IP PROM_PORT VALOPER_ADDRESS WALLET_ADDRESS PROJECT_NAME
+~/cosmos_node_monitoring/add_validator.sh VALIDATOR_IP PROM_PORT VALOPER_ADDRESS WALLET_ADDRESS PROJECT_NAME
 
 # run container
-cd cosmos_node_monitoring/
+cd ~/cosmos_node_monitoring/
 docker-compose up -d
